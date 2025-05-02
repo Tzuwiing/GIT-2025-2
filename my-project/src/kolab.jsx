b21de98013e8a08631873acdc9cf1d5a27da1fcf
WildanCool <wildan2512@student.abudzar.sch.id>
2025-05-02 11:01:14 +0700

    Tambah Person Wildan

--- my-project/src/kolab.jsx
+++ my-project/src/kolab.jsx
@@ -1,3 +1,4 @@
+import FotoWildan from "./assets/PotoWildan.jpg"
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start p-8">
@@ -7,6 +8,9 @@ export default function App() {
         alt="Foto Besar"
         className="w-full max-w-4xl rounded-2xl shadow-lg"
       />
+
+      <h2 className="text-4xl font-bold text-gray-800 mb-6">Punya Wildan</h2>
+      <img src={FotoWildan} alt="" />
     </div>
   );
 }
