db6e950165b006121143a2e58a9fd9faac6eb31f
Tzuwiing <jusuf1909@student.abudzar.sch.id>
2025-05-07 11:30:24 +0700

    g

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./kolab.jsx";
 import LoginPage from "./branch/login.jsx";
+import TaskDashboard from "./merge/dashboard.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LoginPage />
+    <TaskDashboard />
   </StrictMode>
 );
